497214c773e581739b7c016ac517d6c4ad286536
D3fau4 <raulwii2@gmail.com>
2019-07-12 11:39:06 +0200

    Update Request.cpp

--- source/Net/Request.cpp
+++ source/Net/Request.cpp
@@ -67,7 +67,7 @@ bool Net::Download(string url, string filepath) {
     if (curl) {   
         fp = fopen(filepath.c_str(),"wb");
         curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-		curl_easy_setopt(curl, CURLOPT_USERPWD, "CXboS8mjRWZneBS:");
+	//	curl_easy_setopt(curl, CURLOPT_USERPWD, "CXboS8mjRWZneBS:");
         curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
         curl_easy_setopt(curl, CURLOPT_RESOLVE, hosts);
         curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
@@ -118,7 +118,7 @@ bool Net::Downloader1(string url, string filepath) {
     if (curl) {   
         fp = fopen(filepath.c_str(),"wb");
         curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-		curl_easy_setopt(curl, CURLOPT_USERPWD, "NA9CsKnyApss5Lr:");
+	//	curl_easy_setopt(curl, CURLOPT_USERPWD, "NA9CsKnyApss5Lr:");
         curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
         curl_easy_setopt(curl, CURLOPT_RESOLVE, hosts);
         curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
@@ -169,7 +169,7 @@ bool Net::Downloader2(string url, string filepath) {
     if (curl) {   
         fp = fopen(filepath.c_str(),"wb");
         curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-		curl_easy_setopt(curl, CURLOPT_USERPWD, "YAZi5Fe2FEEopGr:");
+//		curl_easy_setopt(curl, CURLOPT_USERPWD, "YAZi5Fe2FEEopGr:");
         curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
         curl_easy_setopt(curl, CURLOPT_RESOLVE, hosts);
         curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
